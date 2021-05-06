63061484c25c64ce522b3b145742f225c488cec4
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-05 22:39:23 -0400

    feat: ensure DbAuthentication calls hash compare and jwt generator

--- backend/src/data/usecases/authentication/auth-authentication.ts
+++ backend/src/data/usecases/authentication/auth-authentication.ts
@@ -1,14 +1,24 @@
 import { AuthenticationModel, UserAuthentication } from '@/domain/usecase/user/user-authentication'
 import { DbLoadAccountByRegistration } from '@/data/protocols/db/db-load-account-by-registration'
-import { User } from '@/domain/model/user'
+import { HashComparer } from '@/data/protocols/criptography/hash-compare'
+import Encrypter from '@/data/protocols/criptography/encrypter'
 
 export class DbAuthentication implements UserAuthentication {
   constructor (
-    private readonly dbLoadAccountByRegistration: DbLoadAccountByRegistration
+    private readonly dbLoadAccountByRegistration: DbLoadAccountByRegistration,
+    private readonly hashComparer: HashComparer,
+    private readonly jwtGenerator: Encrypter
   ) {}
 
-  async auth (auth: AuthenticationModel): Promise<{ token: string, userAccount: Omit<User, 'id' | 'password'> }> {
-    await this.dbLoadAccountByRegistration.loadByRegistration(auth.registration)
+  async auth (auth: AuthenticationModel): Promise<string> {
+    const userData = await this.dbLoadAccountByRegistration.loadByRegistration(auth.registration)
+    if (userData) {
+      const passwordMath = await this.hashComparer.compare(auth.password, userData.password)
+      if (passwordMath) {
+        const token = await this.jwtGenerator.encrypt({ id: userData.id, permission: userData.permission })
+        return token
+      }
+    }
     return null
   }
 }
