9626b0eae7350f75bb519d5d1ce109b56eeeeda7
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-07 22:05:24 -0400

    feat: ensure AuthenticationController execute validation

--- backend/src/presentation/controllers/user/authentication-controller.ts
+++ backend/src/presentation/controllers/user/authentication-controller.ts
@@ -1,16 +1,22 @@
 import { UserAuthentication } from '@/domain/usecase/user/user-authentication'
-import { responseSuccess, serverError } from '@/presentation/protocols/helpers/http-helpers'
+import { badRequest, responseSuccess, serverError } from '@/presentation/protocols/helpers/http-helpers'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { HttpController } from '@/presentation/protocols/http-controller'
+import { Validation } from '@/presentation/protocols/validation'
 
 export class AuthenticationController implements HttpController {
   constructor (
-    private readonly userAuth: UserAuthentication
+    private readonly userAuth: UserAuthentication,
+    private readonly authValidator: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { registration, password } = httpRequest.body
+      const validationError = this.authValidator.validate({ registration, password })
+      if (validationError) {
+        return badRequest(validationError)
+      }
       const token = await this.userAuth.auth({ registration, password })
       return responseSuccess({ token })
     } catch (error) {
