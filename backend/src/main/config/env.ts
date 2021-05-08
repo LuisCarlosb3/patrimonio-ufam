582d92e4a7f196a1e2bb8b810dda8d35c5dc2c90
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-07 22:06:33 -0400

    style: enforce code style

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   databaseURL: process.env.DATABASE_URL || '',
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || '',
+  jwtSecret: process.env.JWT_SECRET || ''
 }
