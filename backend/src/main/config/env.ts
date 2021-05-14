3fe7913ed6c97b8551ad3e8973c9f023f0de8483
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-13 20:39:59 -0400

    chore: setup library to database manager

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  databaseURL: process.env.DATABASE_URL || '',
+  env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || ''
 }
---
+++ backend/src/main/config/knexfile.ts
@@ -0,0 +1,60 @@
+interface KnexConfig {
+  development: object
+  test: object
+  production: object
+}
+const knexConfig: KnexConfig = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: '../../infra/db/helper/migrations'
+    }
+  },
+
+  test: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: '../../infra/db/helper/migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: '../../infra/db/helper/migrations'
+    }
+  }
+
+}
+export default knexConfig
