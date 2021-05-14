5fb870d29322d403697374d5d722d2dc3d0ed14f
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-13 20:50:09 -0400

    chore: define users migration

---
+++ backend/src/infra/db/helper/migrations/20210511011831_users-migration.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  await knex.raw('create extension if not exists "uuid-ossp"')
+  return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()'))
+    table.string('name', 255).notNullable()
+    table.string('registration', 20).notNullable()
+    table.string('email', 100).notNullable()
+    table.string('password', 20).notNullable()
+    table.integer('permission').notNullable()
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+  return knex.raw('drop extension if exists "uuid-ossp"')
+}
