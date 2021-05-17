921d8258eed780049bb98d06e6b246a98fdf42db
Ednilson da Silva Albuquerque Júnior <ednilson.junior@iatec.com>
2021-05-17 20:42:52 -0300

    chore: create pages structure

---
+++ frontend/src/presentation/styles/Layout/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+`;
+
+export const Main = styled.div`
+  background: #f7f7fc;
+  width: 100%;
+
+  padding: 3rem 4rem;
+`;
