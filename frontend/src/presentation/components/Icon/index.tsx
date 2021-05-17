c6af261712d42b93f67c3577edae9ecf600d8bfb
Ednilson da Silva Albuquerque Júnior <ednilson.junior@iatec.com>
2021-05-17 20:41:32 -0300

    chore: create custom icon component

---
+++ frontend/src/presentation/components/Icon/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import iconPath from '../../styles/Icons/iconsLib';
+
+const defaultStyles = { display: 'inline-block', verticalAlign: 'middle' };
+
+interface IconProps {
+  size?: number;
+  color?: string;
+  icon: 'home' | 'search' | 'report';
+  className?: string;
+  viewBox?: string;
+  stroke?: number;
+}
+
+const Icon: React.FC<IconProps> = ({
+  size,
+  color,
+  icon,
+  className,
+  stroke,
+}) => {
+  const styles = { ...defaultStyles };
+
+  return (
+    <svg
+      className={className}
+      style={styles}
+      viewBox="0 0 24 24"
+      width={`${size}px`}
+      height={`${size}px`}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      strokeWidth={stroke}
+    >
+      {JSON.parse(iconPath[icon]).map((item: string) => {
+        return <path key={item} stroke={color} fill="none" d={item} />;
+      })}
+    </svg>
+  );
+};
+
+Icon.defaultProps = {
+  size: 16,
+  color: '#000',
+  viewBox: '0 0 24 24',
+  className: '',
+};
+
+export default Icon;
