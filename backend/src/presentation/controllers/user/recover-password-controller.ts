512015ecd50b025593be0be87e09aaaa11c0d07c
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-18 23:25:19 -0400

    feat: ensure RecoverPasswordController returns badRequest on invalid body request

--- backend/src/presentation/controllers/user/recover-password-controller.ts
+++ backend/src/presentation/controllers/user/recover-password-controller.ts
@@ -1,16 +1,23 @@
 import { UserRecoverPassword } from '@/domain/usecase/user/user-recover-password'
 import { GenerateRecoverPasswordLink } from '@/presentation/protocols/generate-link-service'
-import { noContent, serverError, unauthorizedRequest } from '@/presentation/protocols/helpers/http-helpers'
+import { badRequest, noContent, serverError, unauthorizedRequest } from '@/presentation/protocols/helpers/http-helpers'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { HttpController } from '@/presentation/protocols/http-controller'
+import { Validation } from '@/presentation/protocols/validation'
 
 export class RecoverPasswordController implements HttpController {
   constructor (
     private readonly generateRecoverLink: GenerateRecoverPasswordLink,
-    private readonly dbRecoverUserPassword: UserRecoverPassword) {}
+    private readonly dbRecoverUserPassword: UserRecoverPassword,
+    private readonly validator: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const validationError = this.validator.validate(httpRequest.body)
+      if (validationError) {
+        return badRequest(validationError)
+      }
       const { registration } = httpRequest.body
       const recoverhash = await this.generateRecoverLink.generate(registration)
       const userRecover = await this.dbRecoverUserPassword.recover(registration, recoverhash)
