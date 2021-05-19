b58a021892260adb46800027c6ca807617f93d61
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-18 20:48:00 -0400

    chore: define fake jwtsecret

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || ''
+  jwtSecret: process.env.JWT_SECRET || 'mjwthashsecrete@2021'
 }
