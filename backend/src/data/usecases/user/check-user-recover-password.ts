53450815896baef9016073ef41eeec21ebbeafad
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-26 23:08:29 -0400

    feat: ensure CheckRecoverPassword returns user id on succeeds

--- backend/src/data/usecases/user/check-user-recover-password.ts
+++ backend/src/data/usecases/user/check-user-recover-password.ts
@@ -4,7 +4,12 @@ import { CheckUserRecoverLink } from '@/domain/usecase/user/user-recover-passwor
 export class CheckUserRecoverPassword implements CheckUserRecoverLink {
   constructor (private readonly dbLoadUser: DbLoadUserByRecoverLink) {}
   async verify (link: string): Promise<string> {
-    await this.dbLoadUser.loadByLink(link)
+    const userLink = await this.dbLoadUser.loadByLink(link)
+    if (userLink) {
+      if (userLink.expiresAt.getTime() > new Date().getTime()) {
+        return userLink.userId
+      }
+    }
     return null
   }
 }
