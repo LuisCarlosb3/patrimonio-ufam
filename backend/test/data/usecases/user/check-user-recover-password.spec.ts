7d493057d45875e5a9fac30f4beb20e4aeb8a8ad
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-26 23:09:47 -0400

    test: ensure CheckRecoverPassword returns user id on succeeds

--- backend/test/data/usecases/user/check-user-recover-password.spec.ts
+++ backend/test/data/usecases/user/check-user-recover-password.spec.ts
@@ -4,11 +4,13 @@ import { CheckUserRecoverPassword } from '@/data/usecases/user/check-user-recove
 function makeDbUserByRecoveryLink (): DbLoadUserByRecoverLink {
   class DbLoadUserByRecoverLinkStub implements DbLoadUserByRecoverLink {
     async loadByLink (link: string): Promise<UserRecover> {
+      const expiresAt = new Date()
+      expiresAt.setHours(new Date().getHours() + 1)
       return await Promise.resolve({
         id: 'any_id',
         userId: 'user_id',
         link: 'any_link',
-        expiresAt: new Date()
+        expiresAt: expiresAt
       })
     }
   }
@@ -31,5 +33,29 @@ describe('CheckUserRecoverPassword', () => {
     const loadSpy = jest.spyOn(dbLoadUser, 'loadByLink')
     await sut.verify('any_link')
     expect(loadSpy).toHaveBeenCalledWith('any_link')
+  })
+  test('Ensure sut CheckUserRecoverPassword returns null if dbLoadUserRecover returns null', async () => {
+    const { sut, dbLoadUser } = makeSut()
+    jest.spyOn(dbLoadUser, 'loadByLink').mockResolvedValueOnce(null)
+    const res = await sut.verify('any_link')
+    expect(res).toEqual(null)
+  })
+  test('Ensure sut CheckUserRecoverPassword returns user id on success', async () => {
+    const { sut } = makeSut()
+    const res = await sut.verify('any_link')
+    expect(res).toEqual('user_id')
+  })
+  test('Ensure sut CheckUserRecoverPassword returns null if link has expired', async () => {
+    const { sut, dbLoadUser } = makeSut()
+    const expiresAt = new Date()
+    expiresAt.setHours(new Date().getHours())
+    jest.spyOn(dbLoadUser, 'loadByLink').mockResolvedValueOnce({
+      id: 'any_id',
+      userId: 'user_id',
+      link: 'any_link',
+      expiresAt: expiresAt
+    })
+    const res = await sut.verify('any_link')
+    expect(res).toEqual(null)
   })
 })
