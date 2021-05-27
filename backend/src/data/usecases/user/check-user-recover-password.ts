278bda9d862427b29a2c9cbe1f9b5dc86c7eb4e8
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-26 23:47:15 -0400

    refactor: ensure CheckUserRecover calls delete by id on link already expires

--- backend/src/data/usecases/user/check-user-recover-password.ts
+++ backend/src/data/usecases/user/check-user-recover-password.ts
@@ -1,13 +1,20 @@
+import { DbDeleteUserByRecoverByID } from '@/data/protocols/db/check-user-recover-password/db-delete-user-link'
 import { DbLoadUserByRecoverLink } from '@/data/protocols/db/check-user-recover-password/db-load-user-by-link'
 import { CheckUserRecoverLink } from '@/domain/usecase/user/user-recover-password'
 
 export class CheckUserRecoverPassword implements CheckUserRecoverLink {
-  constructor (private readonly dbLoadUser: DbLoadUserByRecoverLink) {}
+  constructor (
+    private readonly dbLoadUser: DbLoadUserByRecoverLink,
+    private readonly dbDeleteLinkById: DbDeleteUserByRecoverByID
+  ) {}
+
   async verify (link: string): Promise<string> {
     const userLink = await this.dbLoadUser.loadByLink(link)
     if (userLink) {
       if (userLink.expiresAt.getTime() > new Date().getTime()) {
         return userLink.userId
+      } else {
+        await this.dbDeleteLinkById.deleteById(userLink.id)
       }
     }
     return null
--- backend/test/data/usecases/user/check-user-recover-password.spec.ts
+++ backend/test/data/usecases/user/check-user-recover-password.spec.ts
@@ -1,30 +1,45 @@
 import { DbLoadUserByRecoverLink } from '@/data/protocols/db/check-user-recover-password/db-load-user-by-link'
 import { UserRecover } from '@/domain/model/user'
 import { CheckUserRecoverPassword } from '@/data/usecases/user/check-user-recover-password'
+import { DbDeleteUserByRecoverByID } from '@/data/protocols/db/check-user-recover-password/db-delete-user-link'
+const makeFakeLinkData = (expiresAt: Date): UserRecover => {
+  return {
+    id: 'any_id',
+    userId: 'user_id',
+    link: 'any_link',
+    expiresAt: expiresAt
+  }
+}
+
 function makeDbUserByRecoveryLink (): DbLoadUserByRecoverLink {
   class DbLoadUserByRecoverLinkStub implements DbLoadUserByRecoverLink {
     async loadByLink (link: string): Promise<UserRecover> {
       const expiresAt = new Date()
       expiresAt.setHours(new Date().getHours() + 1)
-      return await Promise.resolve({
-        id: 'any_id',
-        userId: 'user_id',
-        link: 'any_link',
-        expiresAt: expiresAt
-      })
+      return await Promise.resolve(makeFakeLinkData(expiresAt))
     }
   }
   return new DbLoadUserByRecoverLinkStub()
 }
+function makeDbDeleteLinkById (): DbDeleteUserByRecoverByID {
+  class DbDeleteUserByRecoverByIDStub implements DbDeleteUserByRecoverByID {
+    async deleteById (linkId: string): Promise<void> {
+      return await Promise.resolve(null)
+    }
+  }
+  return new DbDeleteUserByRecoverByIDStub()
+}
 interface Sut {
   sut: CheckUserRecoverPassword
   dbLoadUser: DbLoadUserByRecoverLink
+  dbDeleteById: DbDeleteUserByRecoverByID
 }
 
 const makeSut = (): Sut => {
   const dbLoadUser = makeDbUserByRecoveryLink()
-  const sut = new CheckUserRecoverPassword(dbLoadUser)
-  return { sut, dbLoadUser }
+  const dbDeleteById = makeDbDeleteLinkById()
+  const sut = new CheckUserRecoverPassword(dbLoadUser, dbDeleteById)
+  return { sut, dbLoadUser, dbDeleteById }
 }
 
 describe('CheckUserRecoverPassword', () => {
@@ -49,13 +64,23 @@ describe('CheckUserRecoverPassword', () => {
     const { sut, dbLoadUser } = makeSut()
     const expiresAt = new Date()
     expiresAt.setHours(new Date().getHours())
-    jest.spyOn(dbLoadUser, 'loadByLink').mockResolvedValueOnce({
-      id: 'any_id',
-      userId: 'user_id',
-      link: 'any_link',
-      expiresAt: expiresAt
-    })
+    jest.spyOn(dbLoadUser, 'loadByLink').mockResolvedValueOnce(makeFakeLinkData(expiresAt))
     const res = await sut.verify('any_link')
     expect(res).toEqual(null)
+  })
+  test('Ensure sut CheckUserRecoverPassword call DbDeleteUserByRecoverByID with link id on link expires', async () => {
+    const { sut, dbLoadUser, dbDeleteById } = makeSut()
+    const expiresAt = new Date()
+    expiresAt.setHours(new Date().getHours())
+    jest.spyOn(dbLoadUser, 'loadByLink').mockResolvedValueOnce(makeFakeLinkData(expiresAt))
+    const loadSpy = jest.spyOn(dbDeleteById, 'deleteById')
+    await sut.verify('any_link')
+    expect(loadSpy).toHaveBeenCalledWith('any_id')
+  })
+  test('Ensure sut CheckUserRecoverPassword dont call DbDeleteUserByRecoverByID with link id on link not expires', async () => {
+    const { sut, dbDeleteById } = makeSut()
+    const loadSpy = jest.spyOn(dbDeleteById, 'deleteById')
+    await sut.verify('any_link')
+    expect(loadSpy).not.toHaveBeenCalled()
   })
 })
