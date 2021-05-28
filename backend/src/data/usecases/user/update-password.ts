eed8f80ce6f2a033560790b65be80f5329aae54f
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-05-27 22:49:42 -0400

    feat: ensure UpdatePassword calls db update password with user id and hash

---
+++ backend/src/data/usecases/user/update-password.ts
@@ -0,0 +1,15 @@
+import { Hasher } from '@/data/protocols/criptography/hasher'
+import { DbUpdateUserPasswordById } from '@/data/protocols/db/db-update-user-password-by-id'
+import { UserUpdatePassword } from '@/domain/usecase/user/user-update-password'
+
+export class UserUpdatePasswordData implements UserUpdatePassword {
+  constructor (
+    private readonly hasher: Hasher,
+    private readonly dbUpdatePassword: DbUpdateUserPasswordById
+  ) {}
+
+  async updatePassword (userId: string, newPassword: string): Promise<void> {
+    const hashPassword = await this.hasher.hash(newPassword)
+    await this.dbUpdatePassword.updateById(userId, hashPassword)
+  }
+}
