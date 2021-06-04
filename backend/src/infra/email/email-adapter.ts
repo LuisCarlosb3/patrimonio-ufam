e51d8fa148f01ce95154e5b46c4113e80e6f3733
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-03 22:58:54 -0400

    feat: ensure EmailSender implements sendNewAccessLink

--- backend/src/infra/email/email-adapter.ts
+++ backend/src/infra/email/email-adapter.ts
@@ -1,7 +1,19 @@
+import SendNewUserAccessLink from '@/data/protocols/email/send-new-user-link'
 import SendRecoverPasswordEmail from '@/data/protocols/email/send-recover-password'
 import Env from '@/main/config/env'
 import { NodemailerHelper } from './helper/nodemailer-helper'
-export class EmailSender implements SendRecoverPasswordEmail {
+export class EmailSender implements SendRecoverPasswordEmail, SendNewUserAccessLink {
+  async sendNewAccessLink (email: string, link: string): Promise<void> {
+    const message = `
+    <html><h2>Bem-vindo ao Oráculo</h2>
+      <p>
+        Acesse este link para realizar o primeiro acesso ao sistema: ${Env.hostname}/firstaccess/${link} 
+      </p>
+    </html>
+    `
+    await NodemailerHelper.sendEmail('Oráculo - First Access', email, message)
+  }
+
   async sendRecover (email: string, newPasswordLink: string): Promise<void> {
     const message = `
     <html><h2>Recuperação de senha</h2>
