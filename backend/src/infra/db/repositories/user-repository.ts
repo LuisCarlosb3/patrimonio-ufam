d4f5ddf840501684539bc8037619ea1638104c2a
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-03 22:11:50 -0400

    feat: ensure loadByEmailAndRegistration returns user data

--- backend/src/infra/db/repositories/user-repository.ts
+++ backend/src/infra/db/repositories/user-repository.ts
@@ -1,9 +1,10 @@
 import { DbLoadAccountByRegistration } from '@/data/protocols/db/user/db-load-account-by-registration'
+import { DbLoadUserByEmailAndRegistration } from '@/data/protocols/db/user/db-load-user-by-email-and-registration'
 import { DbUpdateUserPasswordById } from '@/data/protocols/db/user/db-update-user-password-by-id'
 import { User } from '@/domain/model/user'
 import knex from '../helper/index'
 
-export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUserPasswordById {
+export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUserPasswordById, DbLoadUserByEmailAndRegistration {
   private readonly tableName = 'users'
   async loadByRegistration (registration: string): Promise<User> {
     const data = await knex<User>(this.tableName).where({ registration })
@@ -14,5 +15,10 @@ export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUser
     await knex<User>(this.tableName).update({
       password: newPassword
     }).where({ id: userId })
+  }
+
+  async loadByEmailAndRegistration (email: string, registration: string): Promise<User> {
+    const data = await knex<User>(this.tableName).where({ registration }).orWhere({ email })
+    return data[0] ?? null
   }
 }
