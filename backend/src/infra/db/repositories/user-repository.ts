a7f6a44617916d8ffa9a83a07271bb88175a4e7a
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-03 22:26:07 -0400

    feat: ensure UserRepository creates user corretcly

--- backend/src/infra/db/repositories/user-repository.ts
+++ backend/src/infra/db/repositories/user-repository.ts
@@ -1,10 +1,11 @@
+import { DbCreateNewUser, DbCreateNewUserModel } from '@/data/protocols/db/user/db-create-new-user'
 import { DbLoadAccountByRegistration } from '@/data/protocols/db/user/db-load-account-by-registration'
 import { DbLoadUserByEmailAndRegistration } from '@/data/protocols/db/user/db-load-user-by-email-and-registration'
 import { DbUpdateUserPasswordById } from '@/data/protocols/db/user/db-update-user-password-by-id'
 import { User } from '@/domain/model/user'
 import knex from '../helper/index'
 
-export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUserPasswordById, DbLoadUserByEmailAndRegistration {
+export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUserPasswordById, DbLoadUserByEmailAndRegistration, DbCreateNewUser {
   private readonly tableName = 'users'
   async loadByRegistration (registration: string): Promise<User> {
     const data = await knex<User>(this.tableName).where({ registration })
@@ -20,5 +21,13 @@ export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUser
   async loadByEmailAndRegistration (email: string, registration: string): Promise<User> {
     const data = await knex<User>(this.tableName).where({ registration }).orWhere({ email })
     return data[0] ?? null
+  }
+
+  async create (newUser: DbCreateNewUserModel): Promise<User> {
+    const { name, registration, email, password, permission } = newUser
+    const [createdUser] = await knex<User>(this.tableName).insert({
+      name, registration, email, password, permission
+    }).returning('*')
+    return createdUser
   }
 }
