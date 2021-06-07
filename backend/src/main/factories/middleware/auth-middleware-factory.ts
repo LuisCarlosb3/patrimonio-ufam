61a276bbf7810a34a8340aa039164ed5857d4106
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-07 17:28:12 -0400

    feat: ensure factories create auth middleware

---
+++ backend/src/main/factories/middleware/auth-middleware-factory.ts
@@ -0,0 +1,8 @@
+import { AuthMiddleware } from '@/presentation/middleware/authentication-middleware/auth-middleware'
+import { makeLoadUserByTokenFactory } from '../usecases/auth-middleware/load-yser-by-token-factory'
+
+export function makeAuthMiddleware (role: number): AuthMiddleware {
+  const loadUserByToken = makeLoadUserByTokenFactory()
+  const auth = new AuthMiddleware(loadUserByToken, role)
+  return auth
+}
