d42d86dba61876b730d39fc42db21e8a2f38307f
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-07 15:02:01 -0400

    feat: configure factories to create user by admin

---
+++ backend/src/main/factories/controller/user/admin-create-new-user-factory.ts
@@ -0,0 +1,22 @@
+import { UserPermission } from '@/domain/model/user'
+import { EmailSender } from '@/infra/email/email-adapter'
+import { ValidatorJsAdapter } from '@/infra/validation/validatorjs-adapter'
+import { AdminCreateNewUserController } from '@/presentation/controllers/user/admin-create-new-user-controller'
+import { makeCreateNewUser } from '../../usecases/admin-create-user/create-new-user-factory'
+import { makeCreateNewUserLinkFactory } from '../../usecases/admin-create-user/create-new-user-link-factory'
+
+export function makeAdminCreateNewUserController (): AdminCreateNewUserController {
+  const rules = {
+    name: 'required',
+    registration: 'required',
+    email: 'required|email',
+    permisssion: ['required', { in: [UserPermission.ADMINISTRATOR, UserPermission.INVENTORIOUS] }]
+  }
+
+  const userValidation = new ValidatorJsAdapter(rules)
+  const createNewUser = makeCreateNewUser()
+  const createNewUserLink = makeCreateNewUserLinkFactory()
+  const emailSender = new EmailSender()
+  const adminCreate = new AdminCreateNewUserController(userValidation, createNewUser, createNewUserLink, emailSender)
+  return adminCreate
+}
