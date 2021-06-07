4f7b7411cf00d1dc7d4825c34945813629c62682
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-07 19:48:27 -0400

    refactor: ensure factory create AuthenticationData correctly

--- backend/src/main/factories/usecases/user-authentication/user-authentication-factory.ts
+++ backend/src/main/factories/usecases/user-authentication/user-authentication-factory.ts
@@ -1,6 +1,7 @@
 import { AuthenticationData } from '@/data/usecases/user/auth-authentication'
 import { UserAuthentication } from '@/domain/usecase/user/user-authentication'
 import BcryptAdapter from '@/infra/bcrypt-adapter/bcrypt-adapter'
+import { UserAccessTokenRepository } from '@/infra/db/repositories/user-access-token'
 import { UserRepository } from '@/infra/db/repositories/user-repository'
 import JwtAdapter from '@/infra/jwt-adapter/jwt-adapter'
 import Env from '@/main/config/env'
@@ -8,5 +9,6 @@ export function buildAuthenticationData (): UserAuthentication {
   const dbLoadAccountByRegistration = new UserRepository()
   const hashComparer = new BcryptAdapter(12)
   const jwtGenerator = new JwtAdapter(Env.jwtSecret)
-  return new AuthenticationData(dbLoadAccountByRegistration, hashComparer, jwtGenerator)
+  const userTokenRepository = new UserAccessTokenRepository()
+  return new AuthenticationData(dbLoadAccountByRegistration, hashComparer, jwtGenerator, userTokenRepository)
 }
