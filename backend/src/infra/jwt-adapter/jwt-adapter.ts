f8ffceef881f7e6ec7c90ea365f9a947d3232653
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-07 19:26:01 -0400

    fix: ensure decrypt returns null on error

--- backend/src/infra/jwt-adapter/jwt-adapter.ts
+++ backend/src/infra/jwt-adapter/jwt-adapter.ts
@@ -4,7 +4,17 @@ import jwt from 'jsonwebtoken'
 export default class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) { }
   async decrypt (token: string): Promise<{ id: string, permission: number }> {
-    const payload = await jwt.verify(token, this.secret)
+    const payload = await new Promise<string | object>(resolve => {
+      jwt.verify(token, this.secret, function (err, decoded) {
+        if (err) {
+          resolve(err.name)
+        }
+        resolve(decoded)
+      })
+    })
+    if (typeof (payload) === 'string') {
+      return null
+    }
     return { id: payload?.['id'], permission: payload?.['permission'] }
   }
 
