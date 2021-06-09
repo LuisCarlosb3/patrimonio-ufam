68dcb83211dc234f3bbaa99f5c1eb8dd27a78abc
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-08 22:53:53 -0400

    refactor: ensure patrimony code to be unique

--- backend/src/infra/db/helper/migrations/20210609012429_patrimony.ts
+++ backend/src/infra/db/helper/migrations/20210609012429_patrimony.ts
@@ -4,7 +4,7 @@ export async function up (knex: Knex): Promise<void> {
   await knex.raw('create extension if not exists "uuid-ossp"')
   return await knex.schema.createTable('patrimony', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary()
-    table.string('code', 50).notNullable()
+    table.string('code', 50).notNullable().unique()
     table.text('description').notNullable()
     table.string('state', 20).notNullable()
     table.date('entry_date').notNullable()
