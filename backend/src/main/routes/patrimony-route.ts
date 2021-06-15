7713f6f0f0eef8aa7ce3cb384ba83dce54c72241
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-14 23:15:49 -0400

    feat: ensure express handle list patrimony controller

--- backend/src/main/routes/patrimony-route.ts
+++ backend/src/main/routes/patrimony-route.ts
@@ -3,10 +3,14 @@ import { Router } from 'express'
 import ExpressMiddlewareAdapterFactory from '../factories/adapter/express-middleware-adapter-factory'
 import ExpressRouteAdapterFactory from '../factories/adapter/express-route-adapter-factory'
 import { makeCreateNewPatrimony } from '../factories/controller/patrimony/create-new-patrimony-factory'
+import { makeListPatrimonyController } from '../factories/controller/patrimony/list-patrimony-controller-factory'
 import { makeAuthMiddleware } from '../factories/middleware/auth-middleware-factory'
 
 export default (router: Router): void => {
   router.post('/patrimony/create',
     ExpressMiddlewareAdapterFactory(makeAuthMiddleware(UserPermission.ADMINISTRATOR)),
     ExpressRouteAdapterFactory(makeCreateNewPatrimony()))
+  router.get('/patrimony/:page',
+    ExpressMiddlewareAdapterFactory(makeAuthMiddleware()),
+    ExpressRouteAdapterFactory(makeListPatrimonyController()))
 }
