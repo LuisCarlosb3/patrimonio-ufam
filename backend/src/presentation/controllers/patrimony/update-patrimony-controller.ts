d9a408dd7e51d6c8fb7352624b83cf43545fb0ea
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-16 14:05:05 -0400

    feat: ensure UpdatePatrimonyController calls update with user permission

---
+++ backend/src/presentation/controllers/patrimony/update-patrimony-controller.ts
@@ -0,0 +1,31 @@
+import { UpdatePatrimonyById } from '@/domain/usecase/patrimony/update-patrimony-by-id'
+import { LoadUserById } from '@/domain/usecase/user/load-user-by-id'
+import { badRequest, noContent, serverError } from '@/presentation/protocols/helpers/http-helpers'
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { HttpController } from '@/presentation/protocols/http-controller'
+import { Validation } from '@/presentation/protocols/validation'
+
+export class UpdatePatrimonyController implements HttpController {
+  constructor (
+    private readonly validator: Validation,
+    private readonly loadUserById: LoadUserById,
+    private readonly updatePatrimonyById: UpdatePatrimonyById
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const isValid = this.validator.validate(httpRequest.body)
+      if (isValid) {
+        return badRequest(isValid)
+      }
+      const { patrimony, accountId } = httpRequest.body
+      const { permission } = await this.loadUserById.load(accountId)
+      const isUpdate = await this.updatePatrimonyById.updateById(permission, patrimony)
+      if (isUpdate) {
+        return noContent()
+      }
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
