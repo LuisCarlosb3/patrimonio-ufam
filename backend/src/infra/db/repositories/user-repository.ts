48ec301da0d70414250d31dd8a89a61309f86977
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-16 00:25:01 -0400

    feat: ensure repository returns user on search by

--- backend/src/infra/db/repositories/user-repository.ts
+++ backend/src/infra/db/repositories/user-repository.ts
@@ -1,11 +1,12 @@
 import { DbCreateNewUser, DbCreateNewUserModel } from '@/data/protocols/db/user/db-create-new-user'
 import { DbLoadAccountByRegistration } from '@/data/protocols/db/user/db-load-account-by-registration'
 import { DbLoadUserByEmailAndRegistration } from '@/data/protocols/db/user/db-load-user-by-email-and-registration'
+import { DbLoadUserById } from '@/data/protocols/db/user/db-load-user-by-id'
 import { DbUpdateUserPasswordById } from '@/data/protocols/db/user/db-update-user-password-by-id'
 import { User } from '@/domain/model/user'
 import knex from '../helper/index'
 
-export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUserPasswordById, DbLoadUserByEmailAndRegistration, DbCreateNewUser {
+export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUserPasswordById, DbLoadUserByEmailAndRegistration, DbCreateNewUser, DbLoadUserById {
   private readonly tableName = 'users'
   async loadByRegistration (registration: string): Promise<User> {
     const data = await knex<User>(this.tableName).where({ registration })
@@ -29,5 +30,10 @@ export class UserRepository implements DbLoadAccountByRegistration, DbUpdateUser
       name, registration, email, password, permission
     }).returning('*')
     return createdUser
+  }
+
+  async loadById (id: string): Promise<User> {
+    const data = await knex<User>(this.tableName).where({ id })
+    return data[0] ?? null
   }
 }
