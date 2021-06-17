cd443bb10a0e0aaf64ccb804fb9ceb87cd6b270d
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-16 22:38:56 -0400

    feat: ensure update patrimony insert new itens and update alredy registered itens

--- backend/src/data/usecases/patrimony/update-patrimony-by-id.ts
+++ backend/src/data/usecases/patrimony/update-patrimony-by-id.ts
@@ -1,19 +1,33 @@
 import { DbCheckIfPatrimonyExists } from '@/data/protocols/db/patrimony/db-check-if-patrimony-exists-by-id'
+import { DbInsertNewItensToPatrimony } from '@/data/protocols/db/patrimony/db-insert-new-itens-to-patrimony'
 import { DbUpdatePatrimonyById } from '@/data/protocols/db/patrimony/db-update-patrimony-by-id'
-import { Patrimony } from '@/domain/model/patrimony'
+import { Patrimony, PatrimonyItens } from '@/domain/model/patrimony'
 import { UserPermission } from '@/domain/model/user'
-import { UpdatePatrimonyById } from '@/domain/usecase/patrimony/update-patrimony-by-id'
+import { NewItenToInsert, UpdatePatrimonyById, UpdatePatrimonyModel } from '@/domain/usecase/patrimony/update-patrimony-by-id'
 
 export class UpdatePatrimonyByIdData implements UpdatePatrimonyById {
   constructor (
     private readonly verifyByIdRepository: DbCheckIfPatrimonyExists,
-    private readonly updateByIdRepository: DbUpdatePatrimonyById
+    private readonly updateByIdRepository: DbUpdatePatrimonyById,
+    private readonly insertNewItens: DbInsertNewItensToPatrimony
   ) {}
 
-  async updateById (userPermission: UserPermission, patrimony: Patrimony): Promise<boolean> {
+  async updateById (userPermission: UserPermission, patrimony: UpdatePatrimonyModel): Promise<boolean> {
     const exists = await this.verifyByIdRepository.verifyById(patrimony.id)
     if (exists) {
-      const updateSucceeds = await this.updateByIdRepository.updateById(patrimony)
+      const { deletedItens, ...payload } = patrimony
+      const newItens: NewItenToInsert[] = payload.patrimonyItens.filter(item => {
+        return (!item.id)
+      })
+      const updatedItens = payload.patrimonyItens.filter(item => {
+        return (item.id)
+      })
+      const patrimonyToUpdate: Patrimony = {
+        ...payload,
+        patrimonyItens: updatedItens as PatrimonyItens[]
+      }
+      const updateSucceeds = await this.updateByIdRepository.updateById(patrimonyToUpdate)
+      await this.insertNewItens.insertItens(patrimony.id, newItens)
       if (updateSucceeds === null) {
         return true
       }
