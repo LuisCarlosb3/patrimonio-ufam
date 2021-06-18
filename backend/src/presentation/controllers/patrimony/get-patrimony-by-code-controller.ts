cebd052e95277df397ea4ec29c5fae65f032a56a
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-18 17:08:08 -0400

    feat: ensure GetpatrimonyByCode controller returns patrimony on success

---
+++ backend/src/presentation/controllers/patrimony/get-patrimony-by-code-controller.ts
@@ -0,0 +1,31 @@
+import { LoadPatrimonyByCode } from '@/domain/usecase/patrimony/load-patrimony-by-code'
+import { PatrimonyNotFound } from '@/presentation/protocols/helpers/errors'
+import { badRequest, responseSuccess, serverError } from '@/presentation/protocols/helpers/http-helpers'
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { HttpController } from '@/presentation/protocols/http-controller'
+import { Validation } from '@/presentation/protocols/validation'
+
+export class GetPatrimonyByCodeController implements HttpController {
+  constructor (
+    private readonly loadPatrimony: LoadPatrimonyByCode,
+    private readonly validator: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const isValid = this.validator.validate(httpRequest.params)
+      if (isValid) {
+        return badRequest(isValid)
+      }
+      const { code } = httpRequest.params
+      const patrimony = await this.loadPatrimony.loadByCode(code)
+      if (patrimony) {
+        return responseSuccess({ patrimony })
+      } else {
+        return badRequest(new PatrimonyNotFound())
+      }
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
