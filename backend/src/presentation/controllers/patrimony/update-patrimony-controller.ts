4c1455cce4d198663b2b902b88fc038cfe0fe497
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-18 14:11:38 -0400

    feat: ensure UpdatePatrimonyController verify patrimony before update

--- backend/src/presentation/controllers/patrimony/update-patrimony-controller.ts
+++ backend/src/presentation/controllers/patrimony/update-patrimony-controller.ts
@@ -1,7 +1,6 @@
-import { LoadPatrimonyById } from '@/domain/usecase/patrimony/load-patrimony-by-id'
+import { CheckPatrimonyIdAndCodeToUpdate } from '@/domain/usecase/patrimony/check-patrimony-id-and-code'
 import { UpdatePatrimonyById } from '@/domain/usecase/patrimony/update-patrimony-by-id'
 import { LoadUserById } from '@/domain/usecase/user/load-user-by-id'
-import { PatrimonyNotFound } from '@/presentation/protocols/helpers/errors'
 import { badRequest, noContent, serverError } from '@/presentation/protocols/helpers/http-helpers'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { HttpController } from '@/presentation/protocols/http-controller'
@@ -12,7 +11,7 @@ export class UpdatePatrimonyController implements HttpController {
     private readonly validator: Validation,
     private readonly loadUserById: LoadUserById,
     private readonly updatePatrimonyById: UpdatePatrimonyById,
-    private readonly loadPatrimonyById: LoadPatrimonyById
+    private readonly checkPatrimonyIdAndCode: CheckPatrimonyIdAndCodeToUpdate
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -23,15 +22,16 @@ export class UpdatePatrimonyController implements HttpController {
       }
       const { patrimony, accountId } = httpRequest.body
 
-      const patrimonyLoaded = await this.loadPatrimonyById.laodById(patrimony.id)
-      if (patrimonyLoaded) {
+      const patrimonyLoaded = await this.checkPatrimonyIdAndCode.verifyPatrimony(patrimony.id, patrimony.code)
+      if (!(patrimonyLoaded instanceof Error)) {
         const { permission } = await this.loadUserById.load(accountId)
         const isUpdate = await this.updatePatrimonyById.updateById(permission, patrimony)
         if (isUpdate) {
           return noContent()
         }
+      } else {
+        return badRequest(patrimonyLoaded)
       }
-      return badRequest(new PatrimonyNotFound())
     } catch (error) {
       return serverError(error)
     }
