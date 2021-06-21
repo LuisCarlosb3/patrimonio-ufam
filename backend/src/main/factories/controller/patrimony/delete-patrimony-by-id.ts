820ad9ddf275b96aa51f5382ef4671591c165dce
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-21 12:26:52 -0400

    refactor: ensure validator check if id params is uuid format

--- backend/src/main/factories/controller/patrimony/delete-patrimony-by-id.ts
+++ backend/src/main/factories/controller/patrimony/delete-patrimony-by-id.ts
@@ -5,7 +5,7 @@ import { makeLoadPatrimonyById } from '../../usecases/load-patrimony-by-id/load-
 
 export function makeDeletePatrimonyByIdController (): DeletePatrimonyByIdController {
   const rules = {
-    id: 'required|string'
+    id: ['required', 'string', 'regex:/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i']
   }
   const validator = new ValidatorJsAdapter(rules)
   const loadPatrimony = makeLoadPatrimonyById()
