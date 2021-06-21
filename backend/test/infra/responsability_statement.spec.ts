5ea9e6e9f48a9ea57b854509048f27c8098e55d3
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-21 17:29:28 -0400

    test: ensure each patrimony have only one responsability statement

--- backend/test/infra/responsability_statement.spec.ts
+++ backend/test/infra/responsability_statement.spec.ts
@@ -21,6 +21,18 @@ async function insertPatrimony (code?: String): Promise<string> {
   const [id] = await knex('patrimony').insert(patrimony).returning('id')
   return id
 }
+async function insertSatement (patrimonyId: string): Promise<void> {
+  const [newId] = await knex('responsability_statement').insert({
+    responsible_name: 'any_name',
+    siape: 'any_code',
+    emission_date: new Date()
+  }).returning('id')
+  const item = {
+    patrimony_id: patrimonyId,
+    statement_id: newId
+  }
+  await knex('responsability_statement_itens').insert(item)
+}
 const makeSut = (): ResponsabilityStatementRespositoy => {
   return new ResponsabilityStatementRespositoy()
 }
@@ -54,6 +66,18 @@ describe('ResponsabilityStatementRespositoy', () => {
       expect(item).toBeTruthy()
       expect(item.patrimony_id).toEqual(patrimonyId)
       expect(item.statement_id).toEqual(statement.id)
+    })
+    test('Ensure create method don\'t insert new responsability if patrimony already alocated to another one', async () => {
+      const sut = makeSut()
+      const newStatement = makeFakeInsertNewStatementModel()
+      const patrimonyId = await insertPatrimony()
+      await insertSatement(patrimonyId)
+      newStatement.patrimoniesIds.push(patrimonyId)
+      await sut.create(newStatement)
+      const statement = await knex('responsability_statement')
+      const itens = await knex('responsability_statement_itens')
+      expect(itens.length).toBe(1)
+      expect(statement.length).toBe(1)
     })
   })
 })
