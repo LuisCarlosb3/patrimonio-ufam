72b9046fbb9fc94662cd23ad25acdffb2d18a099
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-21 17:15:31 -0400

    feat: ensure CreateResponsabilityStatementController handle create process correctly

---
+++ backend/src/presentation/controllers/responsability-statement/create-responsability-statement-controller.ts
@@ -0,0 +1,34 @@
+import { LoadPatrimonyByCode } from '@/domain/usecase/patrimony/load-patrimony-by-code'
+import { CreateResponsabilityStatement } from '@/domain/usecase/responsability-statement/create-responsability-statement'
+import { PatrimonyNotFound } from '@/presentation/protocols/helpers/errors'
+import { badRequest, serverError } from '@/presentation/protocols/helpers/http-helpers'
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { HttpController } from '@/presentation/protocols/http-controller'
+import { Validation } from '@/presentation/protocols/validation'
+
+export class CreateResponsabilityStatementController implements HttpController {
+  constructor (
+    private readonly validator: Validation,
+    private readonly loadByCode: LoadPatrimonyByCode,
+    private readonly createStatement: CreateResponsabilityStatement
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const validationErr = this.validator.validate(httpRequest.body)
+      if (validationErr) {
+        return badRequest(validationErr)
+      }
+      const { responsibleName, siapeCode, emissionDate, patrimoniesCode } = httpRequest.body.newStatement
+      for await (const code of patrimoniesCode) {
+        const patrimony = await this.loadByCode.loadByCode(code)
+        if (!patrimony) {
+          return badRequest(new PatrimonyNotFound(code))
+        }
+      }
+      await this.createStatement.create({ responsibleName, siapeCode, emissionDate, patrimoniesCode })
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
