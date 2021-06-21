d5cbc691363992ca19c47b8f08281a356456756b
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-21 17:29:12 -0400

    refactor: ensure each patrimony have only one responsability statement

--- backend/src/infra/db/helper/migrations/20210621191809_responsability_statement_itens.ts
+++ backend/src/infra/db/helper/migrations/20210621191809_responsability_statement_itens.ts
@@ -4,7 +4,7 @@ export async function up (knex: Knex): Promise<void> {
   return knex.schema.createTable('responsability_statement_itens', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary()
     table.uuid('statement_id').notNullable().index().references('id').inTable('responsability_statement').onDelete('CASCADE')
-    table.uuid('patrimony_id').notNullable().index().references('id').inTable('patrimony')
+    table.uuid('patrimony_id').notNullable().unique().index().references('id').inTable('patrimony')
   })
 }
 
