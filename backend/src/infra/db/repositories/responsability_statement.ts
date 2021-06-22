6875fd3a603e487adfeb9f6271844bc58a96c6dc
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-22 11:32:45 -0400

    feat: ensure loadByPatrimonyId returns statementItem on success

--- backend/src/infra/db/repositories/responsability_statement.ts
+++ backend/src/infra/db/repositories/responsability_statement.ts
@@ -1,9 +1,21 @@
 import { DbCreateResponsabilityStatement, InsertNewStatementModel } from '@/data/protocols/db/responsability-statement/db-create-statement'
+import { DbLoadStatementItem } from '@/data/protocols/db/responsability-statement/db-load-statement-item'
+import { StatementItem } from '@/domain/usecase/responsability-statement/check-patrimony-statement-exists'
 import knex from '@/infra/db/helper/index'
 
-export class ResponsabilityStatementRespositoy implements DbCreateResponsabilityStatement {
+export class ResponsabilityStatementRespositoy implements DbCreateResponsabilityStatement, DbLoadStatementItem {
   private readonly tableName = 'responsability_statement'
   private readonly itensTableName = 'responsability_statement_itens'
+  private readonly itensTableNameMapper = {
+    patrimonyId: 'patrimony_id',
+    responsabilityStatementId: 'statement_id'
+  }
+
+  async loadByPatrimonyId (patrimonyId: string): Promise<StatementItem> {
+    const item = await knex(this.itensTableName).select(this.itensTableNameMapper).where({ patrimony_id: patrimonyId })
+    return item[0] || null
+  }
+
   async create (newStatement: InsertNewStatementModel): Promise<void> {
     const { responsibleName, siapeCode, emissionDate, patrimoniesIds } = newStatement
 
