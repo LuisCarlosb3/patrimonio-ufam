6a4c5d46e991bd0dc7f3342b3847c0a1d559be3b
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-22 12:27:25 -0400

    refactor: add code to ResponsabilityStatement

--- backend/src/domain/model/responsability-statement.ts
+++ backend/src/domain/model/responsability-statement.ts
@@ -2,6 +2,7 @@ import { Patrimony } from './patrimony'
 
 export interface ResponsabilityStatement {
   id: string
+  code: string
   responsibleName: string
   siapeCode: string
   emissionDate: Date
