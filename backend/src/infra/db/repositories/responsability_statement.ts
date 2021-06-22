fe9d5344684aa0bc7bf3b320f4b7803609fa1f33
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-22 16:58:49 -0400

    feat: ensure ResponsabilityStatements returns itens peer page correclty

--- backend/src/infra/db/repositories/responsability_statement.ts
+++ backend/src/infra/db/repositories/responsability_statement.ts
@@ -1,10 +1,13 @@
 import { DbCheckIfCodeExists } from '@/data/protocols/db/responsability-statement/db-check-if-code-exists'
 import { DbCreateResponsabilityStatement, InsertNewStatementModel } from '@/data/protocols/db/responsability-statement/db-create-statement'
 import { DbLoadStatementItem } from '@/data/protocols/db/responsability-statement/db-load-statement-item'
+import { DbLoadResponsabilityStatementList } from '@/data/protocols/db/responsability-statement/db-load-statements-list'
+import { ResponsabilityStatement } from '@/domain/model/responsability-statement'
 import { PatrimonyStatementItem } from '@/domain/usecase/responsability-statement/check-patrimony-statement-exists'
 import knex from '@/infra/db/helper/index'
 
-export class ResponsabilityStatementRespositoy implements DbCreateResponsabilityStatement, DbLoadStatementItem, DbCheckIfCodeExists {
+export class ResponsabilityStatementRespositoy implements DbCreateResponsabilityStatement,
+   DbLoadStatementItem, DbCheckIfCodeExists, DbLoadResponsabilityStatementList {
   private readonly tableName = 'responsability_statement'
   private readonly itensTableName = 'responsability_statement_itens'
   private readonly itensTableNameMapper = {
@@ -24,6 +27,35 @@ export class ResponsabilityStatementRespositoy implements DbCreateResponsability
   async verifyCode (code: string): Promise<boolean> {
     const [{ count }] = await knex(this.tableName).where({ code }).count('id')
     return (count > 0)
+  }
+
+  async load (page: number, quantityPeerPage: number): Promise<ResponsabilityStatement[]> {
+    const baseData = await knex(this.tableName).select(this.statementTableNameMapper)
+      .limit(quantityPeerPage).offset(page)
+    const statements = []
+    for await (const item of baseData) {
+      const id = item.id
+      const patrimonies = []
+      const data = await knex(this.itensTableName).where({ statement_id: id })
+        .rightJoin('patrimony', `${this.itensTableName}.patrimony_id`, 'patrimony.id')
+      for (const item of data) {
+        patrimonies.push({
+          id: item.patrimony_id,
+          code: item.code,
+          description: item.description,
+          state: item.state,
+          entryDate: item.entry_date,
+          lastConferenceDate: item.last_conference_date,
+          value: item.value
+        })
+      }
+      const payload = {
+        ...item,
+        patrimonies
+      }
+      statements.push(payload)
+    }
+    return statements
   }
 
   async loadByPatrimonyId (patrimonyId: string): Promise<PatrimonyStatementItem> {
