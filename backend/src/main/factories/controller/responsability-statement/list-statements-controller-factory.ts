b39f3f78dd627a728679dce7d731a24aa0b1dc29
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-24 10:21:33 -0400

    refactor: ensure ListStatementsController be initialize with correct depedencies

--- backend/src/main/factories/controller/responsability-statement/list-statements-controller-factory.ts
+++ backend/src/main/factories/controller/responsability-statement/list-statements-controller-factory.ts
@@ -1,9 +1,11 @@
 import { makeListResponsabilityStatementData } from '../../usecases/responsability-statement/list-responsability-statement/list-statements-factory'
 import { ListResponsabilityStatementController } from '@/presentation/controllers/responsability-statement/list-resposability-statement-controller'
 import { HttpController } from '@/presentation/protocols/http-controller'
+import { makeLoadStatementByCodeOrSiape } from '../../usecases/responsability-statement/load-statement-by-code-or-siape/load-statement-by-code-or-siape'
 
 export function makeListResponsabilityStatementsController (): HttpController {
   const listPatrimonyData = makeListResponsabilityStatementData()
-  const controller = new ListResponsabilityStatementController(listPatrimonyData)
+  const listPatrimonyDataWithFilter = makeLoadStatementByCodeOrSiape()
+  const controller = new ListResponsabilityStatementController(listPatrimonyData, listPatrimonyDataWithFilter)
   return controller
 }
