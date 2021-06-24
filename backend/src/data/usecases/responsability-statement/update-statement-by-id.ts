1c4aa9856e4393025a1adf8090b9d750d7ae8ec5
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-24 15:10:38 -0400

    feat: ensure UpdateStatementData updates statement and patrimony

---
+++ backend/src/data/usecases/responsability-statement/update-statement-by-id.ts
@@ -0,0 +1,17 @@
+import { DbUpdateStatementIdOnPatrimonyById } from '@/data/protocols/db/patrimony/db-update-patrimony-with-statement-id'
+import { DbUpdateStatementById } from '@/data/protocols/db/responsability-statement/db-update-statement-by-id'
+import { UpdateStatementById, UpdateStatementModel } from '@/domain/usecase/responsability-statement/update-statement-by-id'
+
+export class UpdateStatementByIdData implements UpdateStatementById {
+  constructor (
+    private readonly updateStatement: DbUpdateStatementById,
+    private readonly updatePatrimonyWithId: DbUpdateStatementIdOnPatrimonyById
+  ) {}
+
+  async updateById (statement: UpdateStatementModel): Promise<void> {
+    const { addedPatrimonies, removedPatrimonies, ...statementData } = statement
+    await this.updateStatement.updateById(statementData)
+    await this.updatePatrimonyWithId.updateStatement(addedPatrimonies, statementData.id)
+    await this.updatePatrimonyWithId.updateStatement(addedPatrimonies, null)
+  }
+}
