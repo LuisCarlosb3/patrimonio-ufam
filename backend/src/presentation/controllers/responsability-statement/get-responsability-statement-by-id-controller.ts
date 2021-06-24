e703d34d61113ecd155e9224ba13f4a865f54050
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-24 13:29:40 -0400

    fix: adjust controller class name

--- backend/src/presentation/controllers/responsability-statement/get-responsability-statement-by-id-controller.ts
+++ backend/src/presentation/controllers/responsability-statement/get-responsability-statement-by-id-controller.ts
@@ -5,7 +5,7 @@ import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { HttpController } from '@/presentation/protocols/http-controller'
 import { Validation } from '@/presentation/protocols/validation'
 
-export class GetStatementByCodeController implements HttpController {
+export class GetStatementByIdController implements HttpController {
   constructor (
     private readonly loadStatement: LoadStatementById,
     private readonly validator: Validation
