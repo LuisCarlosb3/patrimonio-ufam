d0d0d48618293c6aecb7bb215ff16668159844f4
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-06-25 08:52:03 -0400

    test: ensure DeleteStatement check if statement exists before deletes

--- backend/test/presentation/controllers/responsability-statement/delete-responsability-statement-by-id-controller.spec.ts
+++ backend/test/presentation/controllers/responsability-statement/delete-responsability-statement-by-id-controller.spec.ts
@@ -2,12 +2,24 @@ import { DeleteResponsabilityStatementeByIdController } from '@/presentation/con
 import { DeleteStatementById } from '@/domain/usecase/responsability-statement/delete-statement-by-id'
 import { HttpRequest } from '@/presentation/protocols/http'
 import { badRequest, noContent, serverError } from '@/presentation/protocols/helpers/http-helpers'
-import { StatementHasPatrimony } from '@/presentation/protocols/helpers/errors'
+import { StatementHasPatrimony, StatementNotFound } from '@/presentation/protocols/helpers/errors'
+import { LoadStatementById } from '@/domain/usecase/responsability-statement/load-statement-by-id'
+import { ResponsabilityStatement } from '@/domain/model/responsability-statement'
 const makeHttpRequest = (): HttpRequest => ({
   params: {
     id: 'id'
   }
 })
+const makeResponsabilityStatement = (): ResponsabilityStatement => {
+  return {
+    id: 'any_id',
+    code: 'any_code',
+    responsibleName: 'any_name',
+    siapeCode: 'any_siape',
+    emissionDate: new Date(),
+    patrimonies: []
+  }
+}
 function makeDeleteStatementById (): DeleteStatementById {
   class DeleteStatementByIdStub implements DeleteStatementById {
     async deleteById (statementId: string): Promise<boolean> {
@@ -16,17 +28,28 @@ function makeDeleteStatementById (): DeleteStatementById {
   }
   return new DeleteStatementByIdStub()
 }
+const makeLoadStatementById = (): LoadStatementById => {
+  class LoadStatementByIdStub implements LoadStatementById {
+    async loadById (id: string): Promise<ResponsabilityStatement> {
+      return await Promise.resolve(makeResponsabilityStatement())
+    }
+  }
+  return new LoadStatementByIdStub()
+}
 
 interface Sut {
   sut: DeleteResponsabilityStatementeByIdController
   deleteStatementById: DeleteStatementById
+  loadStatementById: LoadStatementById
 }
 const makeSut = (): Sut => {
   const deleteStatementById = makeDeleteStatementById()
-  const sut = new DeleteResponsabilityStatementeByIdController(deleteStatementById)
+  const loadStatementById = makeLoadStatementById()
+  const sut = new DeleteResponsabilityStatementeByIdController(deleteStatementById, loadStatementById)
   return {
     sut,
-    deleteStatementById
+    deleteStatementById,
+    loadStatementById
   }
 }
 
@@ -47,6 +70,20 @@ describe('DeleteResponsabilityStatementeByIdController', () => {
     jest.spyOn(deleteStatementById, 'deleteById').mockResolvedValueOnce(false)
     const response = await sut.handle(makeHttpRequest())
     expect(response).toEqual(badRequest(new StatementHasPatrimony()))
+  })
+  test('Ensure UpdateStatementController calls loadStatementById with statement id', async () => {
+    const { sut, loadStatementById } = makeSut()
+    const loadSpy = jest.spyOn(loadStatementById, 'loadById')
+    const request = makeHttpRequest()
+    await sut.handle(request)
+    expect(loadSpy).toHaveBeenCalledWith(request.params.id)
+  })
+  test('Ensure UpdateStatementController returns badRequest if loadStatementById returns null', async () => {
+    const { sut, loadStatementById } = makeSut()
+    jest.spyOn(loadStatementById, 'loadById').mockResolvedValueOnce(null)
+    const request = makeHttpRequest()
+    const response = await sut.handle(request)
+    expect(response).toEqual(badRequest(new StatementNotFound()))
   })
   test('ensure controller returns 500 on deleteByIdData throws', async () => {
     const { sut, deleteStatementById } = makeSut()
