d4a286307cc384da28a2584d4482f86df760e29c
Ednilson Albuquerque <ednilson.albuquerque@conexasaude.com.br>
2021-06-30 21:01:09 -0400

    feat: implement auth user

---
+++ frontend/src/infra/http/axios-http-client.ts
@@ -0,0 +1,69 @@
+import axios, { AxiosResponse } from 'axios';
+
+type HttpRequest = {
+  url: string;
+  method: HttpMethod;
+  // eslint-disable-next-line
+  body?: any;
+  // eslint-disable-next-line
+  headers?: any;
+};
+
+// eslint-disable-next-line
+interface HttpClient<R = any> {
+  request: (data: HttpRequest) => Promise<HttpResponse<R>>;
+}
+
+type HttpMethod = 'post' | 'get' | 'put' | 'delete';
+
+enum HttpStatusCode {
+  ok = 200,
+  created = 201,
+  noContent = 204,
+  badRequest = 400,
+  unauthorized = 401,
+  forbidden = 403,
+  notFound = 404,
+  serverError = 500,
+}
+
+const baseUrl = 'http://localhost:3000/';
+
+// eslint-disable-next-line
+type HttpResponse<T = any> = {
+  statusCode: HttpStatusCode;
+  body?: T;
+};
+
+export class AxiosHttpClient implements HttpClient {
+  // eslint-disable-next-line class-methods-use-this
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+
+    try {
+      axiosResponse = await axios.request({
+        url: baseUrl + data.url,
+        method: data.method,
+        data: data.body,
+        headers: data.headers,
+      });
+    } catch (error) {
+      axiosResponse = error.response;
+
+      if (!axiosResponse) {
+        axiosResponse = {
+          data: [],
+          status: 403,
+          statusText: 'Forbidden',
+          headers: '',
+          config: {},
+        };
+      }
+    }
+
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    };
+  }
+}
