ca654bfeb6451731cf63469d0e7f8de7864333de
Thuan <thuansilva14@gmail.com>
2021-07-06 11:37:21 -0400

    chore: form register items

--- frontend/src/presentation/pages/Patrimony/index.tsx
+++ frontend/src/presentation/pages/Patrimony/index.tsx
@@ -1,7 +1,125 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from '@unform/web';
+import {
+  Container,
+  ModalContent,
+  Header,
+  FormGroup,
+  ItemInput,
+  InputForm,
+  Title,
+  Content,
+} from './styles';
+import { ReactComponent as ProfileIcon } from '../../../assets/Profile.svg';
+import { ReactComponent as Close } from '../../../assets/close.svg';
+import Modal from '../../components/Modal';
+
+interface PatrimonyItens {
+  name: string;
+  localization: string;
+  observation: string;
+}
+
+interface RegisterPatrimony {
+  code: string;
+  description: string;
+  state: string;
+  entryDate: string;
+  lastConferenceData: string;
+  value: string;
+  patrimonyItens: Array<PatrimonyItens>;
+}
 
 const Patrimony: React.FC = () => {
-  return <h1>Patrimony</h1>;
+  const [openModalCreate, setOpenModalCreate] = useState(true);
+  return (
+    <Container>
+      <Header>
+        <h1>Patrimônio</h1>
+        <div className="user-data">
+          <div>
+            <ProfileIcon />
+            <h4>Junior Albuquerque</h4>
+          </div>
+
+          <button
+            type="button"
+            onClick={() => {
+              console.log('hello');
+            }}
+          >
+            Desconectar
+          </button>
+        </div>
+      </Header>
+
+      <Modal open={openModalCreate} setOpen={setOpenModalCreate}>
+        <ModalContent>
+          <div className="header">
+            <h1 style={{ color: '#14142B' }}>Cadastrar Item</h1>
+            <Close onClick={() => setOpenModalCreate(!openModalCreate)} />
+          </div>
+
+          <Form onSubmit={() => console.log('oia eu aqui!')}>
+            {/* Primeira aparte  */}
+            <FormGroup>
+              <ItemInput>
+                <Title>Código</Title>
+                <InputForm name="Código" type="text" alt="Código" />
+              </ItemInput>
+              <ItemInput>
+                <Title>Valor</Title>
+                <InputForm name="Valor" type="text" alt="Valor" />
+              </ItemInput>
+              <ItemInput>
+                <Title>Entrada</Title>
+                <InputForm name="Entrada" type="date" alt="Entrada" />
+              </ItemInput>
+            </FormGroup>
+
+            <FormGroup>
+              <ItemInput>
+                <Title>Conferência</Title>
+                <InputForm name="Conferência" type="date" alt="Conferência" />
+              </ItemInput>
+              <ItemInput>
+                <Title>Termo</Title>
+                <InputForm name="Termo" type="text" alt="Termo" />
+              </ItemInput>
+              <ItemInput>
+                <Title>Estado</Title>
+                <InputForm name="Estado" type="text" alt="Estado" />
+              </ItemInput>
+            </FormGroup>
+
+            <ItemInput>
+              <Title>Observação</Title>
+              <InputForm name="observação" type="text" alt="observação" />
+            </ItemInput>
+
+            <ItemInput>
+              <Title>Nome do Item</Title>
+              <InputForm name="Nome_do_Item" type="text" alt="Nome_do_Item" />
+            </ItemInput>
+
+            <ItemInput>
+              <Title>Localização</Title>
+              <InputForm name="localização" type="text" alt="Localização" />
+            </ItemInput>
+            <ItemInput>
+              <Title>Observação</Title>
+              <InputForm name="observação2" type="text" alt="observação2" />
+            </ItemInput>
+          </Form>
+          <Content>
+            <button className="button" type="button">
+              Salvar
+            </button>
+          </Content>
+        </ModalContent>
+      </Modal>
+    </Container>
+  );
 };
 
 export default Patrimony;
--- frontend/src/presentation/pages/Patrimony/styles.ts
+++ frontend/src/presentation/pages/Patrimony/styles.ts
@@ -1,1 +1,181 @@
 import styled from 'styled-components';
+import Input from '../../components/Input';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  gap: 2.8rem;
+  padding: 3rem 4rem;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-weight: 400;
+    font-size: 3rem;
+  }
+
+  .user-data {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 0.4rem;
+
+    div {
+      display: flex;
+      align-items: center;
+
+      h4 {
+        font-size: 2.2rem;
+        margin-left: 0.8rem;
+      }
+    }
+
+    button {
+      background: none;
+      border: 0;
+      color: #946200;
+      font-weight: 600;
+      font-size: 1.8rem;
+
+      cursor: pointer;
+    }
+  }
+`;
+
+export const ModalContent = styled.div`
+  background: #fff;
+  width: auto;
+  max-width: calc(100vh - 2rem);
+  max-height: calc(100vh - 2rem);
+  padding: 3.4rem;
+  border-radius: 2rem;
+  position: relative;
+  overflow-y: auto;
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+      color: #0f0f0f;
+
+      font-weight: bold;
+      font-size: 3.4rem;
+    }
+
+    svg {
+      position: absolute;
+      top: 2rem;
+      right: 2rem;
+
+      cursor: pointer;
+    }
+  }
+
+  .patrimonio {
+    margin-top: 2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2rem 3rem;
+    flex-wrap: wrap;
+    color: #151515;
+
+    h4 {
+      font-size: 1.6rem;
+      color: #151515;
+    }
+
+    p {
+      margin-top: 0.8rem;
+      font-size: 1.4rem;
+    }
+  }
+`;
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 65rem;
+`;
+
+export const ItemInput = styled.div`
+  width: 70rem;
+`;
+
+export const InputForm = styled(Input)`
+  z-index: 1;
+  width: 90%;
+  height: 4rem;
+  background: #eff0f6;
+  color: #14142b;
+  border-radius: 1rem;
+  box-sizing: border-box;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.6rem;
+  line-height: 1.75rem;
+  letter-spacing: 0.045rem;
+  padding: 1.5rem 1.5rem;
+  margin-bottom: 1.4rem;
+
+  :focus-visible {
+    border: 0.125rem solid #2a00a2;
+    background: #fcfcfc;
+  }
+  ::placeholder {
+    color: #a0a3bd;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.5rem;
+  }
+  @media (max-width: 425px) {
+    width: 55vw;
+    font-weight: 4rem;
+  }
+`;
+
+export const Title = styled.p`
+  font-family: Poppins;
+  font-style: normal;
+  /* font-weight: bold; */
+  font-size: 1.5rem;
+  line-height: 3.125rem;
+  letter-spacing: 0.063rem;
+  color: #000000;
+  @media (max-width: 425px) {
+    text-align: left;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .button {
+    width: 15rem;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 28px;
+    background-color: #5f2eea;
+    border-radius: 1.2rem;
+    text-align: center;
+    letter-spacing: 0.75px;
+    color: #f7f7fc;
+    padding: 1.5rem;
+  }
+`;
